f2cf7c16e3174187e8fa2f87aa5d4170a9944fbb
Alumnos <Alumnos@LC01-PC22>
2016-01-13 12:32:59 -0500

    agrege Person.java

--- Or-ate1/src/pe/edu/upeupooclass/class1/Person.java
+++ Or-ate1/src/pe/edu/upeupooclass/class1/Person.java
@@ -18,9 +18,9 @@ public class Person {
 
 	@Override
 	public String toString() {
-		return "[nombre=\t" + nombre + "\n apellido=" + apellido + "\n dni=" + dni + "\n email=" + email
-				+ "\n religion=" + religion + "\n sexo=" + sexo + "\n celular=" + celular + "\n talla=" + talla + "\n fecha="
-				+ fecha + "\n vivo=" + vivo + "\n edad=" + edad + "]";
+		return "[nombre=\t" + nombre + "\n apellido=\t" + apellido + "\n dni=\t" + dni + "\n email=\t" + email
+				+ "\n religion=\t" + religion + "\n sexo=\t" + sexo + "\n celular=\t" + celular + "\n talla=\t" + talla + "\n fecha=\t"
+				+ fecha + "\n vivo=\t" + vivo + "\n edad=\t" + edad + "]";
 	}
 	
 }
